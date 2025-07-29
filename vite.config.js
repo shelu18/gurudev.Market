46057bb59720b2f21f711fc267b6aee3b64da9a8
shelu18 <officialshailendraparihar18@gmail.com>
2025-07-29 11:30:12 +0530

    fix: Update base URL in vite.config.js to root path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/gurudev.Market/' // Use your repo name here
+  base: '/' // Use your repo name here
 });
